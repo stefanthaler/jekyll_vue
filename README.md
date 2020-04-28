962f9453f17e2f4f76451fdddbfbebc51d2fb3f1
Stefan <bruthaler@protonmail.com>
2020-04-28 17:48:26 +0200

    Adds some todos

--- README.md
+++ README.md
@@ -6,9 +6,16 @@ Jekyll plugin to use single component vue files in your jekyll blog.
 
 # Usage
 
+
+
 # Todo
 - Create head tag for app
 - Pre Build hook for moving js files
 - Pre Build hook for moving css files
 - Pre Build hook for moving img files
 - Pre Build hook for invoking yarn build
+
+- Tag for component libary inclusion (production, development)
+- Pre build hook for component library compilation
+- Install folders, install vue
+- Support for both NPM, and yarn
--- lib/jekyll_vue.rb
+++ lib/jekyll_vue.rb
@@ -1,6 +1,7 @@
 require 'fileutils'
 
 # TODO get asset dir from vue config js
+# TODO failsafe if some folders are not existing
 
 module Jekyll
   module VueAssetFilter
@@ -39,10 +40,13 @@ module Jekyll
           out_tag += "<link href='/assets/vue/js/#{js_file}' rel='prefetch' as='script'>\n"
         end
       end
-      Dir.entries("./assets/vue/css").each do |css_file|
-          next if css_file =="." or css_file==".."
-          out_tag +=  "<link href='/assets/vue/css/#{css_file}' rel='preload' as='style'>\n"
-          out_tag +=  "<link href='/assets/vue/css/#{css_file}' rel='stylesheet'>\n"
+
+      if File.exist?("./assets/vue/css")
+        Dir.entries("./assets/vue/css").each do |css_file|
+            next if css_file =="." or css_file==".."
+            out_tag +=  "<link href='/assets/vue/css/#{css_file}' rel='preload' as='style'>\n"
+            out_tag +=  "<link href='/assets/vue/css/#{css_file}' rel='stylesheet'>\n"
+        end
       end
       out_tag
     end
@@ -55,12 +59,14 @@ module Jekyll
     end
 
     def render(context)
+      out_tag = ""
       Dir.entries("./assets/vue/js").each do |js_file|
         next if js_file =="." or js_file==".." or js_file.include? ".map"
         if js_file.include? "app." or js_file.include? "chunk-vendors."
-          out_tag += "<script href='/assets/vue/js/#{js_file}' ></script>\n"
+          out_tag += "<script src='/assets/vue/js/#{js_file}' ></script>\n"
         end
       end
+      out_tag
     end
   end
 end
@@ -73,7 +79,14 @@ Liquid::Template.register_filter(Jekyll::VueAssetFilter)
 
 Jekyll::Hooks.register :site, :after_init do |site|
   # call yarn build
-  `yarn build`
+
+  yarn_out = `yarn build`
+  exit_code = `echo $?`
+  if exit_code.to_i != 0
+    puts exit_code
+    puts yarn_out # TODO use logging facility
+    raise 'Yarn build failed'
+  end
 
   # delete old assets/img folder (only works on )
   #files = Dir.entries("").filter! {|x| x == "." || x =='.." }
